a4e939c5e18d6706a68e149a2b24cbcc36c3d156
timdams <dams.tim@telenet.Be>
2017-01-24 19:30:54 +0100

    Some error logging from msbuild

--- FastSolutionEvaluator/MainWindow.xaml.cs
+++ FastSolutionEvaluator/MainWindow.xaml.cs
@@ -21,6 +21,8 @@ using Microsoft.Win32;
 using System.Windows.Markup;
 using Microsoft.Build.Evaluation;
 using Microsoft.Build.Execution;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
 
 namespace FastSolutionEvaluator
 {
@@ -274,6 +276,8 @@ namespace FastSolutionEvaluator
             globalProperty.Add("OutputPath", @"c:\temp");
 
             BuildParameters bp = new BuildParameters(pc);
+            MSBuildLogger customLogger = new MSBuildLogger();
+            bp.Loggers = new List<ILogger>() { customLogger };
             BuildRequestData buildRequest = new BuildRequestData(projectFilePath, globalProperty, "4.0", new string[] { "Build" }, null);
             // THIS IS WHERE THE MAGIC HAPPENS - IN PROCESS MSBUILD
             BuildResult buildResult = BuildManager.DefaultBuildManager.Build(bp, buildRequest);
@@ -285,8 +289,57 @@ namespace FastSolutionEvaluator
             }
             else
             {
-                MessageBox.Show("nope");
+                lbLog.Items.Insert(0, customLogger.BuildErrors);
             }
+        }
+    }
+
+    class MSBuildLogger:Logger
+    {
+        private StringBuilder errorLog = new StringBuilder();
+
+        public string BuildErrors { get; private set; }
+
+        /// <summary>
+        /// This will gather info about the projects built
+        /// </summary>
+        public IList<string> BuildDetails { get; private set; }
+
+        /// <summary>
+        /// Initialize is guaranteed to be called by MSBuild at the start of the build
+        /// before any events are raised.
+        /// </summary>
+        public override void Initialize(IEventSource eventSource)
+        {
+            BuildDetails = new List<string>();
+
+            // FOR BREVITY, WE'LL ONLY REGISTER FOR CERTAIN EVENT TYPES.
+            eventSource.ProjectStarted += new ProjectStartedEventHandler(eventSource_ProjectStarted);
+            eventSource.ErrorRaised += new BuildErrorEventHandler(eventSource_ErrorRaised);
+
+        }
+
+        void eventSource_ErrorRaised(object sender, BuildErrorEventArgs e)
+        {
+            // BUILDERROREVENTARGS ADDS LINENUMBER, COLUMNNUMBER, FILE, AMONGST OTHER PARAMETERS
+            string line = String.Format(": ERROR {0}({1},{2}): ", e.File, e.LineNumber, e.ColumnNumber);
+            errorLog.Append(line + e.Message);
+        }
+
+        void eventSource_ProjectStarted(object sender, ProjectStartedEventArgs e)
+        {
+            BuildDetails.Add(e.Message);
+        }
+
+        /// <summary>
+        /// Shutdown() is guaranteed to be called by MSBuild at the end of the build, after all
+        /// events have been raised.
+        /// </summary>
+        public override void Shutdown()
+        {
+            // DONE LOGGING, LET GO OF THE FILE
+            BuildErrors = errorLog.ToString();
+
         }
     }
 
